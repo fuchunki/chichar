80be80e70dc730b90a9e782b0ed0c016ff49a97b
fuchunki <68526425+fuchunki@users.noreply.github.com>
2025-09-13 15:10:37 +0800

    Update list.js

--- list.js
+++ list.js
@@ -1,3 +1,6 @@
         const images = [
-            '一.jpg','太陽.jpg','筆.jpg','籃球.jpg','電腦.jpg','香蕉.jpg'
+                '冰箱.jpg',  '杯子.jpg',  '沙發.jpg',  '窗簾.jpg',  '耳機.jpg' , '鑰匙.jpg' , '餐桌.jpg'
+'床.jpg'  ,  '桌子.jpg' , '燈.jpg' ,   '筷子.jpg' , '錢包.jpg',  '電腦.jpg'
+'手機.jpg' , '椅子.jpg' , '碗.jpg'  ,  '紙.jpg'  , '鏡子.jpg' , '電視.jpg'
+
         ];
