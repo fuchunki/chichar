07e9e5ef17721d5108fb9df18699d8eafd4bd00e
fuchunki <68526425+fuchunki@users.noreply.github.com>
2025-09-17 14:05:59 +0800

    Update list.js for randomly drawn words

--- list.js
+++ list.js
@@ -1,5 +1,9 @@
 const images = [
-'冰箱.jpg','杯子.jpg','沙發.jpg','窗簾.jpg','耳機.jpg',
-'鑰匙.jpg','餐桌.jpg','床.jpg','桌子.jpg','燈.jpg',
-'筷子.jpg','錢包.jpg','電腦.jpg','手機.jpg','椅子.jpg','碗.jpg','紙.jpg','鏡子.jpg','電視.jpg'
+'冰箱.jpg',	'杯子.jpg',	'沙發.jpg',	'窗簾.jpg',	'耳機.jpg',
+'鑰匙.jpg',	'餐桌.jpg',	'床.jpg',	'桌子.jpg',	'燈.jpg',
+'筷子.jpg',	'錢包.jpg',	'電腦.jpg',	'手機.jpg',	'椅子.jpg',
+'碗.jpg',	'紙.jpg',	'鏡子.jpg',	'電視.jpg'
 ];
+
+const urlParams = new URLSearchParams(window.location.search);
+const NUM_WORDS = parseInt(urlParams.get('num')) || 10;
